9e335c4ccd8e9c6d62ba39429a15d22aa793adc6
Dane M Goodman <DaneGoodman85@gmail.com>
2020-03-08 15:36:31 -0700

    Finally got this deployed

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="rmdb-header">
             <div className="rmdb-header-content">
                 <Link to="/">
-                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo" />
+                <img className="rmdb-logo" src="./images/Movie_Logo.jpg" alt="rmdb-logo" />
                 </Link>
                 <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo" />
             </div>
