ae5dc774b557a2aed910473df5a562e1de589a4a
Dane M Goodman <DaneGoodman85@gmail.com>
2020-03-08 15:44:18 -0700

    I changed a few things and ran another build to host the changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3570ba4591cd13cb877ba9b73215d6ec.js"
+  "flamboyant-mccarthy-f221b3.netlify.com/precache-manifest.60c01c406a879a6074f816ec7c9e6dea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("flamboyant-mccarthy-f221b3.netlify.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
